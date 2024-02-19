978d82f13b5d6ca009677b73b20cc86051c1f9aa
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-02-19 16:27:15 +0200

    updated the index.js file

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 const cors = require('cors');
 app.use(express.json());
+app.use(cors())
 
 
 mongoose.connect( "mongodb+srv://ccnwaka1988:YN8Hg6PhDme4HQI6@mypagedb0.arn4ch4.mongodb.net/newmypagedb?retryWrites=true&w=majority");
