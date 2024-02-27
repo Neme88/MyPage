984f7f3a79df99920c9163bcfe83901f3c817c8f
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-02-27 21:02:17 +0200

    implemented the signout feature

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+
+  const signout = () => {
+    setCookies("access_token", "");
+    window.localStorage.clear();
+    navigate("/signin");
+  };
     return (
         <div className="navbar">
             <Link to="/">Home</Link>
@@ -10,6 +20,11 @@ export const Navbar = () => {
             <Link to="/updatePost">Update Post</Link>
             <Link to="/deletePost">Delete Post</Link>
             <Link to="/readPosts">Read Posts</Link>
+            {!cookies.access_token ? (
+        <Link to="/signin">Signin</Link>
+      ) : (
+        <button onClick={signout}> Signout </button>
+      )}
         </div>
     );
 };
