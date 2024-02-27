d2fc3b31163773fb22ea98b6e1da058b0d1ff97d
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-02-27 11:56:27 +0200

    implemented the Register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,3 +1,44 @@
-export const Register= () => {
-    return <div> Register</div>;
-  };
+//import './App.css';
+import { useState } from "react";
+
+export const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="register-container">
+      <form>
+        <h2> Register </h2>
+        <div className="form-group">
+           <label htmlFor="username">Username: </label>
+           <input 
+           type="text" 
+           id="username" 
+           value={username}
+           onChange={(event) => setUsername(event.target.value)}
+           />
+        </div>
+        <div className="form-group">
+           <label htmlFor="email"> Email: </label>
+           <input 
+           type="text" 
+           id="email" 
+           value={email}
+           onChange={(event) => setEmail(event.target.value)}
+           />
+        </div>
+        <div className="form-group">
+           <label htmlFor="password"> Password: </label>
+           <input 
+           type="text" 
+           id="password" 
+           value={password}
+           onChange={(event) => setPassword(event.target.value)}
+           />
+        </div>
+        <button type="submit"> Register</button>
+      </form>
+    </div>
+  );
+};
